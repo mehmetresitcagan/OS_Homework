2f7576c349e63cdf04b82c08f48cb8c32e8fa719
mehmetresitcagan <69200378+mehmetresitcagan@users.noreply.github.com>
2022-05-25 13:37:30 +0300

    Add files via upload

---
+++ deneme.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int read_txt(char *filename){
+    
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s", filename);
+        return 1;
+    }
+
+    // read one character at a time and
+    // display it to the output
+    char ch;
+    while ((ch = fgetc(fp)) != EOF)
+        putchar(ch);
+
+    // close the file
+    fclose(fp);
+
+
+    return 1;
+}
+
+
+
+int main(){
+    
+    read_txt("opentestcases/matrix1a.txt");
+
+    return 0;
+}
